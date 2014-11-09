97260647b627a9d65ff82e50bd59c3b794e5b171
Steven Traversi <straversi@berkeley.edu>
2014-11-08 16:45:22 -0800

    Removed Old Files
    Lots of Old Files

--- content.js
+++
@@ -1,8 +0,0 @@
-// CONTENT PAGE for steven.education/cs10
-// This is possibly the second most annoying way to write html behind
-// finding the appropriate tabs themselves in the DOM tree.
-// YOU'RE WELCOME
-
-// @author: Steven Traversi
-
-// $( #tab-101 ~ .content ).append("<p class='code'>hello</p>");
