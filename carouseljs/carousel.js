2bebfc18eba1d150ac9fd51066babce7c3063fe2
Steven Traversi <straversi@fanatics.com>
2015-08-12 10:51:25 -0700

    Complete carousel basics testing

--- carouseljs/carousel.js
+++ carouseljs/carousel.js
@@ -15,17 +15,60 @@
 var Carousel = function(element) {
   this.element = element;
   this.pieces = childrenWithClass(this.element, 'carousel-piece');
+  for (var i = 0; i < this.pieces.length; i++) {
+    this.pieces[i].childNumber = i;
+  }
   this.currentPiece = 0;
   this.hammer = new Hammer(this.element, {velocity:0.001,threshold:0});
   var myself = this;
   this.hammer.on('panmove', function(e) {
-    console.log(e);
-    myself.pieces[myself.currentPiece].style.left = e.deltaX.toString() + "px";
-    // console.log(myself.pieces[myself.currentPiece].style.left);
+    updateActiveThreePositions(myself, e.deltaX, false);
   });
   this.hammer.on('panend', function(e) {
-    myself.pieces[myself.currentPiece].style.left = "0px";
-  })
+    if (Math.abs(e.velocityX) > 0.2) {
+      myself.cycle(e.velocityX > 0 ? 1 : -1);
+    } else if (Math.abs(e.deltaX) > myself.element.offsetWidth / 2) {
+      myself.cycle(e.deltaX > 0 ? -1 : 1);
+    } else {
+      myself.cycle(0);
+    }
+  });
+}
+Carousel.prototype.cycle = function(n) {
+  // Flip n pieces. N may be negative or positive.
+  this.currentPiece += n;
+  if (this.currentPiece < 0) { this.currentPiece = 0; }
+  else if (this.currentPiece > this.pieces.length - 1) { this.currentPiece = this.pieces.length - 1; }
+  updateActiveThreePositions(this, 0, true);
+}
+function updateActiveThreePositions(carousel, leftOffset, smooth) {
+  leftOffset += -1 * carousel.element.offsetWidth * carousel.currentPiece;
+  var active = carousel.currentPiece;
+  updatePosition(carousel.pieces[carousel.currentPiece], leftOffset, smooth);
+  if (carousel.currentPiece > 0) {
+    var previousNumber = active - 1;
+    updatePosition(carousel.pieces[previousNumber], leftOffset, smooth);
+  } else if (carousel.currentPiece < carousel.pieces.length - 1) {
+    var nextNumber = active + 1;
+    updatePosition(carousel.pieces[nextNumber], leftOffset, smooth);
+  }
+}
+function updatePosition(element, leftOffset, smooth) {
+  if (smooth) {
+    element.style.setProperty("-webkit-transition", "left .1s linear");
+    element.style.setProperty("-moz-transition", "left .1s linear");
+    element.style.setProperty("-o-transition", "left .1s linear");
+    element.style.setProperty("transition", "left .1s linear");
+  }
+  element.style.left = leftOffset.toString() + "px";
+  if (smooth) {
+    window.setTimeout(function() {
+      element.style.setProperty("-webkit-transition", "none");
+      element.style.setProperty("-moz-transition", "none");
+      element.style.setProperty("-o-transition", "none");
+      element.style.setProperty("transition", "none");
+    }, 100);
+  }
 }
 
 function childrenWithClass(element, targetClass) {
@@ -42,7 +85,7 @@ function childrenWithClass(element, targetClass) {
 // Application
 carousels = document.getElementsByClassName('carousel');
 
-function applyFuncOnEach(func, collection) {
+function map(func, collection) {
   var result = [];
   for (var i = 0; i < collection.length; i++) {
     result[i] = func(collection[i]);
@@ -50,4 +93,4 @@ function applyFuncOnEach(func, collection) {
   return result;
 }
 
-carousels = applyFuncOnEach(function(e) {return new Carousel(e)}, carousels);
+carousels = map(function(e) {return new Carousel(e)}, carousels);
