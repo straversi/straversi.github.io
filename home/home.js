5dc8b5ea880a6a31ecb9aa9f8334037123c5c4db
Steven Traversi <straversi@berkeley.edu>
2015-12-19 22:46:25 -0800

    Rework a life amateur tip description

--- home/home.js
+++ home/home.js
@@ -3,7 +3,7 @@ function pickBackupTip() {
     {title:"Use Your Microwave as a Timer", description:"Did you know that your microwave can time things? Search how to use your microwave's 'timer' functionality on the <a href='http://www.google.com'>internet</a> to get started."},
     {title:"Use Flash on Your Camera at Night", description:"Are your pictures too dark? Try using the 'flash' functionality next time you take a photo of your friends."},
     {title:"Apples Make Good Banana Substitutes", description:"Did you run out of Bananas? An Apple is another common fruit that you could use instead."},
-    {title:"Bored? Use a Television", description:"Bored? Try watching entertainment television. It is designed specifically to be fun to watch."},
+    {title:"Bored? Use a Television", description:"If you're bored, try watching entertainment television. It is designed specifically to be fun to watch."},
     {title:"Use a Couch or Bed to Nap When Tired", description:"If you're feeling tired, try a brief nap. A couch or bed make great locations for falling asleep. Try using your microwave to set a timer so you don't sleep for too long!"},
     {title:"Substitute Pepsi for Coca-Cola", description:"If the restaurant you are eating at does not have any Coca-Cola in stock, try asking for a Pepsi instead. They taste similar."}
     // ["",""],
