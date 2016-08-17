944fa5abddfc497f98aabbef4bae847a33cce53b
Steven Traversi <straversi@berkeley.edu>
2016-08-16 23:55:39 -0700

    try redirect without slash

--- mobile.js
+++ mobile.js
@@ -1,6 +1,6 @@
 if ((/iPhone|iPod|Android|BlackBerry/).test(navigator.userAgent)) {
-    document.location = "/blog";
+    document.location = "/blog/";
 } else {
   /* For now */
-  document.location = "/blog";
+  document.location = "/blog/";
 }
