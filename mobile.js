d9b55f40455372a10b86c1d872f2ba7a6c7a5fd2
Steven Traversi <straversi@berkeley.edu>
2016-08-16 23:52:34 -0700

    redirect index to blog.

--- mobile.js
+++ mobile.js
@@ -1,3 +1,6 @@
 if ((/iPhone|iPod|Android|BlackBerry/).test(navigator.userAgent)) {
     document.location = "/blog/index.html";
+} else {
+  /* For now */
+  document.location = "/blog/index.html";
 }
