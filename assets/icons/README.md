415f24b45c3ca6ddd2457b56419e2dc81d3e4892
Steven Traversi <straversi@berkeley.edu>
2016-11-10 21:18:51 -0800

    remove everything again

--- assets/icons/README.md
+++
@@ -1,9 +0,0 @@
-http://vorillaz.github.io/devicons/#/main
-
-swift
-css3
-python
-
-http://fizzed.com/oss/font-mfizz/icon-javascript
-
-javascript
--- mobile.js
+++
@@ -1,6 +0,0 @@
-if ((/iPhone|iPod|Android|BlackBerry/).test(navigator.userAgent)) {
-  document.location = "/blog/";
-} else {
-  /* For now */
-  document.location = "/blog/";
-}
