9a0fdcfe7de992f13c43cbc1ffc357b9ef4306ac
Steven Traversi <straversi@berkeley.edu>
2018-09-14 14:43:41 -0700

    clean up oscilloscope code

--- oscilloscope/js/main.js
+++ oscilloscope/js/main.js
@@ -1,30 +1,29 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 scene.add(camera);
 
 camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 2.5;
 camera.lookAt(new THREE.Vector3(0, 0, 0));
-
 var CAM_FACE_2D = camera.quaternion.clone();
 
 camera.position.x = 0;
 camera.position.y = 2;
 camera.position.z = 2.5;
 camera.lookAt(new THREE.Vector3(0, 0, 0));
-
 var CAM_FACE_3D = camera.quaternion.clone();
 var CAM_POS_3D = camera.position.clone();
 
 function webglAvailable() {
 	try {
-		var canvas = document.createElement( 'canvas' );
-		return !!( window.WebGLRenderingContext && (
-			canvas.getContext( 'webgl' ) ||
-			canvas.getContext( 'experimental-webgl' ) )
+		var canvas = document.createElement('canvas');
+		return !!(window.WebGLRenderingContext && (
+			canvas.getContext('webgl' ) ||
+			canvas.getContext('experimental-webgl'))
 		);
-	} catch ( e ) {
+	} catch (e) {
 		return false;
 	}
 }
@@ -34,10 +33,10 @@ if (!webglAvailable()) {
 }
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableZoom = false;
 controls.enablePan = false;
 controls.autoRotate = true;
@@ -46,12 +45,15 @@ controls.autoRotateSpeed = -1.0;
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener('resize', onWindowResize, false);
 
-// stats.js r8 - http://github.com/mrdoob/stats.js
+/*
+ * stats.js r8 - http://github.com/mrdoob/stats.js
+ */
+
 var Stats=function(){var h,a,n=0,o=0,i=Date.now(),u=i,p=i,l=0,q=1E3,r=0,e,j,f,b=[[16,16,48],[0,255,255]],m=0,s=1E3,t=0,d,k,g,c=[[16,48,16],[0,255,0]];h=document.createElement("div");h.style.cursor="pointer";h.style.width="80px";h.style.opacity="0.9";h.style.zIndex="10001";h.addEventListener("mousedown",function(a){a.preventDefault();n=(n+1)%2;n==0?(e.style.display="block",d.style.display="none"):(e.style.display="none",d.style.display="block")},!1);e=document.createElement("div");e.style.textAlign=
 "left";e.style.lineHeight="1.2em";e.style.backgroundColor="rgb("+Math.floor(b[0][0]/2)+","+Math.floor(b[0][1]/2)+","+Math.floor(b[0][2]/2)+")";e.style.padding="0 0 3px 3px";h.appendChild(e);j=document.createElement("div");j.style.fontFamily="Helvetica, Arial, sans-serif";j.style.fontSize="9px";j.style.color="rgb("+b[1][0]+","+b[1][1]+","+b[1][2]+")";j.style.fontWeight="bold";j.innerHTML="FPS";e.appendChild(j);f=document.createElement("div");f.style.position="relative";f.style.width="74px";f.style.height=
 "30px";f.style.backgroundColor="rgb("+b[1][0]+","+b[1][1]+","+b[1][2]+")";for(e.appendChild(f);f.children.length<74;)a=document.createElement("span"),a.style.width="1px",a.style.height="30px",a.style.cssFloat="left",a.style.backgroundColor="rgb("+b[0][0]+","+b[0][1]+","+b[0][2]+")",f.appendChild(a);d=document.createElement("div");d.style.textAlign="left";d.style.lineHeight="1.2em";d.style.backgroundColor="rgb("+Math.floor(c[0][0]/2)+","+Math.floor(c[0][1]/2)+","+Math.floor(c[0][2]/2)+")";d.style.padding=
@@ -61,16 +63,19 @@ a.style.width="1px",a.style.height=Math.random()*30+"px",a.style.cssFloat="left"
 stats = new Stats();
 stats.domElement.style.position = 'absolute';
 stats.domElement.style.top = '0px';
+
 if (webglAvailable()) {
 	document.body.appendChild(stats.domElement);
 }
 
-// Box
+/*
+ * Box
+ */
 
-var geometry = new THREE.BoxGeometry( 2, 2, 2 );
-var material = new THREE.MeshBasicMaterial( {color: 0x555555, wireframe: true} );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.BoxGeometry(2, 2, 2);
+var material = new THREE.MeshBasicMaterial({color: 0x555555, wireframe: true});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 var CUBE_FACE_2D = cube.quaternion.clone();
 
@@ -81,70 +86,64 @@ var QUAT_FACE_2D = quatCapturer.quaternion.clone();
 quatCapturer.lookAt(0, 0, 1);
 var QUAT_FACE_3D = quatCapturer.quaternion.clone();
 
-
-// Line
+/*
+ * Line
+ */
 
 var lineGeometry = new THREE.Geometry();
 for (var _ = 0; _ < 1024; _++) {
-	lineGeometry.vertices.push(new THREE.Vector3( 0, 0, 0));
+	lineGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
 }
-uniforms = {
 
-	opacity:   { value: 1.0 },
-
+var uniforms = {
+	opacity: { value: 1.0 },
+	color: { value: new THREE.Vector3(0, 0, 0) }
 };
-var shaderMaterial = new THREE.ShaderMaterial( {
-
+var shaderMaterial = new THREE.ShaderMaterial({
 	uniforms:       uniforms,
-	vertexShader:   document.getElementById( 'vertexshader' ).textContent,
-	fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+	vertexShader:   document.getElementById('vertexshader').textContent,
+	fragmentShader: document.getElementById('fragmentshader').textContent,
 	blending:       THREE.AdditiveBlending,
 	depthTest:      false,
 	transparent:    true
-
 });
 
 var vertices = lineGeometry.vertices;
 
 var buffergeometry = new THREE.BufferGeometry();
 
-var position = new THREE.Float32BufferAttribute( vertices.length * 3, 3 ).copyVector3sArray( vertices );
-buffergeometry.addAttribute( 'position', position );
+var position = new THREE.Float32BufferAttribute(vertices.length * 3, 3).copyVector3sArray(vertices);
+buffergeometry.addAttribute('position', position);
 
-var customColor = new THREE.Float32BufferAttribute( vertices.length * 3, 3 );
-buffergeometry.addAttribute( 'customColor', customColor );
+var customColor = new THREE.Float32BufferAttribute(vertices.length * 3, 3);
+buffergeometry.addAttribute('customColor', customColor);
 
-var color = new THREE.Color( 0xffffff );
+var color = new THREE.Color(0xffffff);
 
-for( var i = 0, l = customColor.count; i < l; i ++ ) {
-
-	color.setHSL( 0.3, 1, 1 - i / l );
-	color.toArray( customColor.array, i * customColor.itemSize );
-
+for (var i = 0, l = customColor.count; i < l; i++) {
+	color.setHSL(0.3, 1, 1 - i / l);
+	color.toArray(customColor.array, i * customColor.itemSize);
 }
 
-// var lineMaterial = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 1, } );
-var line = new THREE.Line( buffergeometry, shaderMaterial );
-cube.add( line );
+var line = new THREE.Line(buffergeometry, shaderMaterial);
+cube.add(line);
 
-// Animation
+/*
+ * Animation
+ */
 
 function animate(timeStamp) {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
 	applyWave(line, timeStamp);
 
 	TWEEN.update(timeStamp);
 	controls.update();
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 
   stats.update();
 }
-
-//
-// Animation details
-//
 
 var xHz = 276;
 var yHz = 460;
@@ -153,22 +152,18 @@ var HzToVizHz = 1/160;
 var masterOffset = 0.1;
 
 function applyWave(line, timeStamp) {
-	// uniforms.color.value.offsetHSL( 0.0005, 0, 0 );
-
 	var attributes = line.geometry.attributes;
 	var positions = attributes.position.array;
 
 	timeStamp = timeStamp || 0;
 
 	var offset = 0;
-	for ( var i = 0, l = positions.length; i < l; i += 3 ) {
-
-		positions[ i     ] = Math.sin(xHz * HzToVizHz * (timeStamp - offset));
-		positions[ i + 1 ] = Math.cos(yHz * HzToVizHz * (timeStamp - offset));
-		positions[ i + 2 ] = Math.sin(zHz * HzToVizHz * (timeStamp - offset));
+	for (var i = 0, l = positions.length; i < l; i += 3) {
+		positions[i] = Math.sin(xHz * HzToVizHz * (timeStamp - offset));
+		positions[i + 1] = Math.cos(yHz * HzToVizHz * (timeStamp - offset));
+		positions[i + 2] = Math.sin(zHz * HzToVizHz * (timeStamp - offset));
 
 		offset += masterOffset;
-
 	}
 
 	attributes.position.needsUpdate = true;
@@ -194,7 +189,6 @@ function switchTo2d() {
 		});
 	tweenA.chain(tweenC, tweenD);
 	tweenA.start();
-
 }
 
 function switchTo3d() {
@@ -217,23 +211,11 @@ function switchTo3d() {
 		.to({ x: 0, y: 2, z: 2.5 }, 1000);
 	tweenC.chain(tweenB, tweenA);
 	tweenC.start();
-
 }
 
-//
-// End of animation details
-//
-
-function randRange(min, max) {
-  return Math.random() * (max - min) + min;
-}
-function randFrom(lst) {
-	return lst[Math.floor(Math.random() * lst.length)];
-}
-
-//
-// Synth and GUI
-//
+/*
+ * Synth and GUI
+ */
 
 var synthX = new Tone.Synth().toMaster();
 var synthY = new Tone.Synth().toMaster();
@@ -242,6 +224,7 @@ var synthZ = new Tone.Synth().toMaster();
 if (webglAvailable()) {
 	var gui = new dat.GUI();
 }
+
 var controller;
 controller = gui.add(window, 'xHz', 0, 600).onChange(function(hz) {
 	synthX.setNote(hz);
@@ -301,7 +284,6 @@ ex.domElement.parentElement.getElementsByClassName('property-name')[0].innerHTML
 function example4() { setAllHz(161.8, 122.7, 284.8); }
 ex = presetsFolder.add(window, 'example4');
 ex.domElement.parentElement.getElementsByClassName('property-name')[0].innerHTML = 'example 4';
-
 
 animate();
 
